c2515afa9461eee4c0e8a0d50e3ecbb4e7cf14d0
arturazambuja <arturantoniolli@gmail.com>
2024-06-04 23:26:57 -0300

    api attributes

--- users/cmd/api/models/checkout.go
+++ users/cmd/api/models/checkout.go
@@ -1,1 +1,7 @@
 package models
+
+type Checkout struct {
+	ID      string `json:"id"`
+	IDCar   string `json:"idCar"`
+	IDUser  string `json:"idUser"`
+}
--- users/cmd/api/models/product.go
+++ users/cmd/api/models/product.go
@@ -1,1 +1,13 @@
 package models
+
+type Product struct {
+	ID       string  `json:"id"`
+	Model    string  `json:"model"`
+	Category string  `json:"category"`
+	Year     int     `json:"year"`
+	Brand    string  `json:"brand"`
+	Km       string  `json:"km"`
+	Collor   string  `json:"collor"`
+	Motor    string  `json:"motor"`
+	Price    float64 `json:"price"`
+}
